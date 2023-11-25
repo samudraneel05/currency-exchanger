6b5682874dfbff84dc6ee5055b9a836717f6133f
Samudraneel Sarkar <98489891+samudraneel05@users.noreply.github.com>
2023-11-25 10:03:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Fetch the list of supported currencies
-    fetch('https://openexchangerates.org/api/currencies.json')
+    fetch('https://v6.exchangerate-api.com/v6/a0f12b49e4749744599c87f9/codes')
         .then(response => response.json())
         .then(data => {
-            const currencies = data;
+            const currencies = data.supported_codes;
 
             // Get the select elements for base and target currencies
             const baseCurrencySelect = document.querySelector('#baseCurrency');
             const targetCurrencySelect = document.querySelector('#targetCurrency');
 
             // Populate the dropdown options with currencies
-            for (const currencyCode in currencies) {
+            currencies.forEach(currency => {
                 // Create option element
                 const option = document.createElement('option');
-                option.value = currencyCode;
-                option.text = `${currencyCode} - ${currencies[currencyCode]}`;
+                option.value = currency[0];
+                option.text = `${currency[0]} - ${currency[1]}`;
 
                 // Append option to both selects
                 baseCurrencySelect.add(option.cloneNode(true));
                 targetCurrencySelect.add(option);
-            }
+            });
         })
         .catch(error => {
             console.error('Error fetching currency list:', error);
@@ -30,11 +30,18 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault(); // Prevent default form submission
 
         const amount = document.querySelector('#amount').value;
+
+        // Check if the amount is not negative
+        if (amount < 0) {
+            document.querySelector('#result').innerHTML = 'Amount cannot be negative.';
+            return;
+        }
+
         const baseCurrency = document.querySelector('#baseCurrency').value;
         const targetCurrency = document.querySelector('#targetCurrency').value;
 
         // Fetch the exchange rates
-        fetch(`https://openexchangerates.org/api/latest.json?app_id=6c6db4703b43478b8812e79257a017f5&base=${baseCurrency}`)
+        fetch(`https://v6.exchangerate-api.com/v6/a0f12b49e4749744599c87f9/latest/${baseCurrency}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
@@ -42,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 return response.json();
             })
             .then(data => {
-                const exchangeRate = data.rates[targetCurrency];
+                const exchangeRate = data.conversion_rates[targetCurrency];
                 if (exchangeRate !== undefined) {
                     const convertedAmount = (amount * exchangeRate).toFixed(3);
                     document.querySelector('#result').innerHTML = `${amount} ${baseCurrency} is equal to ${convertedAmount} ${targetCurrency}`;
